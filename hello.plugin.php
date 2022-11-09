1a0c8265882201659844bba2af7534a7bdcf4141
Waris Agung Widodo <ido.alit@gmail.com>
2022-11-09 11:54:55 +0700

    add name

--- hello.plugin.php
+++ hello.plugin.php
@@ -1,2 +1,11 @@
 <?php
+/**
+ * Plugin Name: Hellow World
+ * Plugin URI: https://github.com/slims/slims9_bulian
+ * Description: Sample plugin with composer
+ * Version: 0.0.2
+ * Author: Waris Agung Widodo
+ * Author URI: https://github.com/idoalit
+ */
+
 \SLiMS\Plugins::menu('opac', 'Hello', __DIR__ . 'index.php');
